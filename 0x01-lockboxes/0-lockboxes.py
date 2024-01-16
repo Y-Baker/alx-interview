8ca527ac4ca719dd99f881970a9189b37cc39139
Yousef <yuossefbakier@gmail.com>
2024-01-16 21:00:35 +0200

    FINISH LockBoxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -8,25 +8,23 @@ def canUnlockAll(boxes):
         True: all boxes can be opened
         False: not all boxes can be opened
     '''
-    length = len(boxes)
-    keys = set()
-    opened_boxes = []
-    i = 0
 
-    while i < length:
-        oldi = i
-        opened_boxes.append(i)
-        keys.update(boxes[i])
-        for key in keys:
-            if key != 0 and key < length and key not in opened_boxes:
-                i = key
-                break
-        if oldi != i:
-            continue
-        else:
+    opened = [False] * len(boxes)
+    opened[0] = True
+    keys = set(boxes[0])
+
+    while True:
+        newKeys = []
+        if len(keys) == 0:
             break
+        for key in keys:
+            if not opened[key]:
+                opened[key] = True
+                newKeys += boxes[key]
 
-    for i in range(length):
-        if i not in opened_boxes and i != 0:
+        keys = set(newKeys)
+
+    for box in opened:
+        if not box:
             return False
     return True
