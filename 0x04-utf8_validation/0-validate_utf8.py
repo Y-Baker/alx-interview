2bad93dc824c418ffc696d7909633f329e811cd4
Yousef <yuossefbakier@gmail.com>
2024-02-23 14:31:41 +0200

    Add Task 0 IN utf8_validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """determines if a given data set represents a valid UTF-8 encoding"""
 
+
 def validUTF8(data):
     """function to determines if  a valid UTF-8 encoding"""
     for i in data:
