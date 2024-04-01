67dec2fc42531a161f8adc3bd189462163724d7d
Yousef <yuossefbakier@gmail.com>
2024-04-01 18:13:23 +0200

    Add Task 0 IN starwars_api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,23 +1,37 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
-    throw error;
+    console.error(error);
+    return;
   }
-  const cast = JSON.parse(body).characters;
-  extract(cast, 0);
+
+  try {
+    const cast = JSON.parse(body).characters;
+    extract(cast, 0);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
+  }
 });
 
 const extract = (cast, i) => {
   if (i === cast.length) {
     return;
   }
+
   request(cast[i], function (error, response, body) {
     if (error) {
-      throw error;
+      console.error(error);
+      return;
     }
-    console.log(JSON.parse(body).name);
-    extract(cast, i + 1);
+
+    try {
+      console.log(JSON.parse(body).name);
+      extract(cast, i + 1);
+    } catch (parseError) {
+      console.error('Error parsing JSON:', parseError);
+    }
   });
 }
