ac03165c78e1a7fce96f3f7d9559115a8c7d84ab
Yousef <yuossefbakier@gmail.com>
2024-04-01 17:48:47 +0200

    Add Task 0 IN starwars_api

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    throw error;
+  }
+  const cast = JSON.parse(body).characters;
+  extract(cast, 0);
+});
+
+const extract = (cast, i) => {
+  if (i === cast.length) {
+    return;
+  }
+  request(cast[i], function (error, response, body) {
+    if (error) {
+      throw error;
+    }
+    console.log(JSON.parse(body).name);
+    extract(cast, i + 1);
+  });
+}
